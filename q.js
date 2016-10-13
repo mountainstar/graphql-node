180bc23630171145fc8ea9c20951673149af1d5e
Lovisa Svallingson <lovisa.svallingson@ca.com>
2016-10-12 21:47:51 -0600

    query examples

---
+++ q.js
@@ -0,0 +1,58 @@
+// Prompts
+// # 1 logged in user and following
+//
+// # 2 view repositoryOwner and their repos
+// 	# (pass in user)
+// 	# cursor, pagination
+
+// 1
+
+query {
+  viewer {
+    login
+  }
+}
+
+// 2
+
+query($login:String!) {
+  repositoryOwner(login:$login) {
+    repositories(last:30) {
+      edges {
+        cursor
+        node {
+          name
+        }
+      }
+    }
+  }
+}
+
+
+
+// 3
+
+{
+  viewer {
+    organizations(first: 1) {
+      edges {
+        node {
+          repositories(last: 10) {
+            edges {
+              node {
+                name
+                pullRequests(last: 10) {
+                  edges {
+                    node {
+                      title
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
